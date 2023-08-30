afcb2912a588a0df9c78f707ab95e0e845d5cf87
degual <danielawa53@gmail.com>
2023-08-30 14:43:21 -0300

    about ui update

--- src/compnent/about/about.js
+++ src/compnent/about/about.js
@@ -16,50 +16,40 @@
  const About = () => {
     return(
         <div class="about-section" id="about">
-            <div className="abt-head-cnt">
-                <div className="abt-head2">
-                <h1>About me</h1>
+            <div className="abt-cnt">
+                <div className="abt-head">
+                    <h3>About me</h3>
+                </div>
+            {/* {*****************} */} 
+                <div className='text'>
+                <p>
+                I'm a seasoned professional who has dedicated a 
+                significant portion of his career to the dynamic 
+                world of marketing and sales. However, my true 
+                passion lies in the field of data analytics. 
+                Throughout my career, I've recognized the 
+                transformative power of data and its impact 
+                on business outcomes.
+                </p>
+
+                <p><br />
+                More so, I aspire to leverage my expertise in 
+                marketing and sales to drive innovation, optimize 
+                operations, and unlock untapped opportunities through 
+                comprehensive data analysis.To learn, explore and excel 
+                is my passion.
+                </p>
+
+                <p><br />
+                I'm eager to dive into the significant and intellectually 
+                stimulating world of data analytics, where I envision myself 
+                as an invaluable asset to organizations, making informed 
+                decisions and driving success through data-driven strategies.
+                </p>
+                
                 </div>
             </div>
-            {/* {*****************} */}
-            <div className='abt-txt-cnt'>
-                <div className='abt-txt1'>
-                    <h3> 
-                        I'm a seasoned professional who has dedicated 
-                        a significant portion of his career to the dynamic
-                        world of marketing and sales. However, my true passion
-                        lies in the field of data analytics. Throughout my career,
-                        I've recognized the transformative power of data and its 
-                        impact on business outcomes.
-                    </h3>
 
-                    <h3><br /> 
-                    Soft Skills:- Empathy, Collaborative Great attention to detail <br /> 
-
-                    🛠 Digital Tools, Type Form, Figma
-                    </h3>
-                </div>
-
-
-                {/* {*****************} */}
-                <div className='abt-txt2'>
-
-                    <h3>
-                         More so, I aspire to leverage my expertise in marketing
-                               and sales to drive innovation, optimize operations,
-                               and unlock untapped opportunities through comprehensive data analysis.
-
-                        <br /> To learn, explore and excel is my passion.
-                    </h3>
-                    <h3><br /> 
-                    I'm eager to dive into the significant and intellectually stimulating 
-                    world of data analytics, where I envision myself as an invaluable asset 
-                    to organizations, making informed decisions and driving success through data-driven strategies.
-                    </h3>
-                </div>
-
-
-            </div>
         </div>
     )
  }

d0e025cddc7a0cdb087216240bcf5c978d486b57
degual <danielawa53@gmail.com>
2023-08-31 13:24:42 -0300

    connect ui update

--- src/compnent/contact/contact.js
+++ src/compnent/contact/contact.js
@@ -1,43 +1,65 @@
 import './contact.css'
-// import connectpic from './image/connect.webp' 
+import resume from '../image/iconoir_arrow-tr.png' 
 // import background from  './image/dot-about3.png'
 // import bb from './image/connect.jpg'
 
 
 const Contact = () => {
     return(
-        <div className="contactsec"  id="contact" style={{
-            // backgroundImage: `url(${background})`,
-            // backgroundRepeat:'no-repeat'
-        }}>
-            <div className='connect-head'>
-                {/* <img src={pic1}></img> */}
-            </div>
-            <div className='connect-cont'>
-                <div className='cont-text1'>
-                    <h3>
-                    I am ready to embark on a journey that will not only 
-                    fulfill my lifetime passion but also open doors to 
-                    endless possibilities.
-                    </h3>
 
-                    {/* <div className='cont-link'>
-                        <div> <a href="#" target="_blank"> <i class="fa-brands fa-github"></i></a>Github</div>
-                    </div>
-                    <div><a href="#" target="_blank"><i class="fa-brands fa-linkedin"></i>linkedin</a></div> */}
-                    <div className='cont-link'> 
-                    <a href="#" target="_blank"> <i class="fa-brands fa-github"></i> <p>Github</p> </a>
-                   <a href="#" target="_blank"><i class="fa-brands fa-linkedin"></i><p>Linkedin</p></a>
-                   <a href="#" target="_blank"> <i class="fa-brands fa-x-twitter"></i><p>Twitter</p></a>
-                   <a href="#" target="_blank"> <i class="fa-brands fa-whatsapp"></i><p>Whatsapp</p></a> 
+        <div className='contactsec'>
+            <div className='contact-cont'>
+                <div className='contact-text'>
+                    <div className='text1'>
+                        <h2>
+                        <span>Open </span>
+                        to interesting challenges 
+                        and opportunities
+                        </h2>
                     </div>
 
+
+                    <div className='text2'>
+                        <div className='text2-1'>
+                            <div><p>Email</p></div>
+
+                                <div className='email'>
+                                    <a href='#'>
+                                    Isaacmagnus@gmail.com
+                                    </a>
+                                </div>
+                        </div>
+
+
+                        <div className='text2-2'> 
+                            <div><p>Social</p></div>
+
+                            <div className='Social'>
+                                <a href='#'> Github</a>
+                                <a href='#'> Linkedin</a>
+                                <a href='#'> twitter</a>
+                                <a href='#'> Whahtsapp</a>
+                            </div>  
+                        </div>
+
+                        <div className='text2-3'> 
+                            <div><p>Resume</p></div>
+
+                            <div className='Social'>
+                                <p> Download Resume</p>  
+                            </div>  
+                            <div><a href='#'><img src={resume}></img> </a></div>
+                        </div>
+                    </div>
                 </div>
-                <div className='pic2'>
-                    {/* <img className='con-pic' src={connectpic}></img> */}
+                {/* ****** */}
+                <div className='img-contact'>
+       
                 </div>
+ 
             </div>
         </div>
+
     )
 }
 export default Contact

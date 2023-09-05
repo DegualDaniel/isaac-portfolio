f3961ec9e858eb6254c292bfe9d77467aeaf27fd
degual <danielawa53@gmail.com>
2023-09-05 09:45:02 -0300

    removed sec commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-// import './App.css';  
-// import Header from './compnent/header/head';   
-// import Home from './compnent/home/home'
-// import About from './compnent/about/about';
-// import Project  from './compnent/project/project';
-// import Service from './compnent/service/service'
-// import Contact from './compnent/contact/contact';
+import './App.css';  
+import Header from './compnent/header/head';   
+import Home from './compnent/home/home'
+import About from './compnent/about/about';
+import Project  from './compnent/project/project';
+import Service from './compnent/service/service'
+import Contact from './compnent/contact/contact';
 
  
 
-// function App() {
-//   return ( 
-//     <div>
+function App() {
+  return ( 
+    <div>
 
 
-//       <Header />
-//       <Home />
-//       <Project />
-//       <Service />
-//       <About />
-//       <Contact />
-//     </div>
-//   );
-// }
+      {/* <Header /> */}
+      {/* <Home /> */}
+      {/* <Project /> */}
+      {/* <Service /> */}
+      {/* <About /> */}
+      {/* <Contact /> */}
+    </div>
+  );
+}
 
-// export default App;
+export default App;

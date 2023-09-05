8b6c49902f0906adab895261287fa6f9db07bdf3
degual <danielawa53@gmail.com>
2023-09-04 22:56:42 -0300

    cost edit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import './App.css'; 
-// import { Routes, Route } from 'react-router-dom';
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Header from './compnent/header/head';  
-// import logopic from './image/favicon.ico.png'
-import Home from './compnent/home/home'
-import About from './compnent/about/about';
-import Project  from './compnent/project/project';
-import Service from './compnent/service/service'
-import Contact from './compnent/contact/contact';
+// import './App.css';  
+// import Header from './compnent/header/head';   
+// import Home from './compnent/home/home'
+// import About from './compnent/about/about';
+// import Project  from './compnent/project/project';
+// import Service from './compnent/service/service'
+// import Contact from './compnent/contact/contact';
 
  
 
-function App() {
-  return ( 
-    <div>
+// function App() {
+//   return ( 
+//     <div>
 
 
-      <Header />
-      <Home />
-      <Project />
-      <Service />
-      <About />
-      <Contact />
-    </div>
-  );
-}
+//       <Header />
+//       <Home />
+//       <Project />
+//       <Service />
+//       <About />
+//       <Contact />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
